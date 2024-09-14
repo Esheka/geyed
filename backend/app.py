7015227db4c640cced369c6e78eed553d666a3f2
tarun25 <tarun25@vt.edu>
2024-09-14 15:30:23 -0400

    docker now maybe

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 # Initialize the Flask app
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 # Define a route for the home page
 @app.route('/')
