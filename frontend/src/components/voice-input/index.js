3f22e637fa46fd44c33d805f995136c176c7c78e
Esheka Garg <eshekag@hawthorn.rlogin>
2024-09-14 14:08:18 -0400

    voice input working

--- frontend/src/components/voice-input/index.js
+++ frontend/src/components/voice-input/index.js
@@ -1,15 +1,31 @@
 import React, { useState } from 'react'
+import useSpeechToText from '../../hooks/useSpeechToText';
 
 const VoiceInput = () => {
     const [textInput, setTextInput] = useState('');
+
+    const { isListening, transcript, startListening, stopListening } = useSpeechToText({ continuous: true })
+
+    const startStopListening = () => {
+        isListening ? stopVoiceInput() : startListening()
+    }
+
+    const stopVoiceInput = () => {
+        setTextInput(prevVal => prevVal + (transcript.length ? (prevVal.length ? ' ' : '') + transcript : ''))
+        stopListening()
+    }
+
     return (
         <div style={{ display: 'block' }}>
-            <button>
+            <button onClick={startStopListening}>
                 Speak
             </button>
             <textarea style={{ width: '100%', height: '150px' }}
-                value={textInput}
+                disabled={isListening}
+                value={isListening ? textInput + (transcript.length ? (textInput.length ? ' ' : '') + transcript : '') : textInput}
                 onChange={(e) => { setTextInput(e.target.value) }} />
         </div>
     )
 }
+
+export default VoiceInput;
