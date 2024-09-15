48bb3789179b75e316c944d62d692d0baf97ce0f
Esheka Garg <eshekag@hawthorn.rlogin>
2024-09-15 00:43:39 -0400

    tts working from openai

--- frontend/src/components/webcam/captureImage.js
+++ frontend/src/components/webcam/captureImage.js
@@ -1,3 +1,4 @@
+
 export const captureImage = async (webcamRef, setImgSrc, text, apiKey) => {
   const imageSrc = webcamRef.current.getScreenshot();
   setImgSrc(imageSrc);
@@ -35,4 +36,25 @@ export const captureImage = async (webcamRef, setImgSrc, text, apiKey) => {
 
   const response = await gpt.json();
   console.log(response);
+  const messageContent = response.choices[0].message.content;
+  console.log(messageContent);
+  textToSpeech(messageContent);
 };
+
+function textToSpeech(text) {
+  const synth = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance(text);
+  const voices = synth.getVoices();
+
+  utterance.voice = voices[2];
+  utterance.pitch = 1;
+  utterance.rate = 1.1;
+  utterance.volume = 1;
+
+  // Automatically start speaking the text
+  synth.speak(utterance);
+
+  return () => {
+    synth.cancel();
+  };
+}
